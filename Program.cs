11c9b9c96291cf82f6986ea66f72b831ce43bf98
Serge <serezhkas1989@gmail.com>
2023-03-30 23:05:30 -0700

    Task13 added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Task13. 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void GiveMeTheThirdDigitPieceOfIronGarbage(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine($"Your number {num} is not three-digit.\nPlease restart the programm and enter another number");
+    }
+    else
+    {
+        int tempNum = num;
+        int length = num.ToString().Length;
+        int[] arr = new int[length];
+        for (int i = 0; tempNum > 0; i++)
+        {
+            arr[i] = tempNum % 10;
+            tempNum = tempNum / 10;
+        }
+
+        Console.WriteLine($"Your number is {num}.\nThe third digit is {arr[arr.Length - 3]}");
+    }
+}
+
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+GiveMeTheThirdDigitPieceOfIronGarbage(num);
+
